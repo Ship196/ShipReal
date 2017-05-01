822ad72f5d4bf873131294d3d7d6e1fbaf46cbe4
vivek2398 <vivek2@illinois.edu>
2017-04-30 21:44:16 -0500

    Update

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, flash, request, url_for, redirect, session
-from create_object import User_list, User, read
+from create_object import User_list, User, read, Profile
 from wtforms import Form, BooleanField, TextField, PasswordField, validators
 from passlib.hash import sha256_crypt
 from MySQLdb import escape_string as thwart
@@ -12,7 +12,7 @@ import gc
 TOPIC = "Big Dicks"
 
 app = Flask(__name__)
-
+app.secret_key = 'super secret key'
 
 class RegistrationForm(Form):
         username = TextField('Username', [validators.Length(min=4, max=20)])
@@ -43,13 +43,40 @@ def login_required(f):
 def homepage():
 	return render_template("index.html")
 
-@app.route('/dashboard', methods=["POST", "GET"])
+@app.route('/dashboard', methods=["GET", "POST"])
 @login_required
 def dashboard():
-    if request.method == "GET":
-        return render_template("dashboard.html", TOPIC = TOPIC)
-    else: 
-        return "hello world"
+    #if request.method = "POST":
+    #    new_val = User(())
+    user_stuff, profile_stuff = read(session['email'])
+
+
+
+    if request.method == "POST":
+        profile_stuff.update_nickname(request.form['nickname'])
+        #print(request.form['nickname'])
+        profile_stuff.update_year(request.form['year'])
+        profile_stuff.update_dorm(request.form['residencehall'])
+        profile_stuff.update_gender(request.form['gender'])
+        profile_stuff.update_interestedIn(request.form['horny'])
+        profile_stuff.update_major(request.form['major'])
+        profile_stuff.update_secondMajor(request.form['secondarymajor'])
+        profile_stuff.update_minor(request.form['minor'])
+        profile_stuff.update_location(request.form['spendtime'])
+        profile_stuff.update_cityOrgin(request.form['city'])
+        profile_stuff.update_stateOrigin(request.form['state'])
+        profile_stuff.update_countryOrigin(request.form['country'])
+        profile_stuff.update_music(request.form['music'])
+        profile_stuff.update_eats(request.form['greenstreeteats'])
+        profile_stuff.update_about(request.form['aboutme'])
+
+        profile_stuff.update_profile()
+        #c.execute("INSERT INTO users (firstname, lastname, email, birthday, password) VALUES (%s, %s, %s, %s, %s)",
+        #(thwart(firstname), thwart(lastname), thwart(email), thwart(birthday), thwart(password)))
+        #c.execute("INSERT INTO profile (nickname, year) values (%s, %s)", (thwart(nickname), thwart(year)))
+        #conn.commit()
+        print("This went well")
+    return render_template("dashboard.html", TOPIC = TOPIC)
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -79,7 +106,7 @@ def login_page():
                 session['email'] = request.form['email']
 
                 flash("You are now logged in")
-                return render_template("dashboard.html")
+                return redirect(url_for("dashboard"))
             else:
                 error = "Invaild credentials. Try again."
                 return render_template("login.html", error = error)
@@ -109,16 +136,16 @@ def register_page():
         print(firstname, lastname)
         x = c.execute("SELECT * FROM users WHERE email = (%s)",
         (email,))
+        print("checks email")
         if int(x) > 0:
             flash("That email has already been taken, please choose another.")
+            print("email Takken")
             return render_template("login.html", form = form)
 
         else:
-            c.execute("INSERT INTO users (firstname, lastname, email, birthday, password) VALUES (%s, %s, %s, %s, %s)",
-            (thwart(firstname), thwart(lastname), thwart(email), thwart(birthday), thwart(password)))
-
-            conn.commit()
-
+            data = User((0,firstname, lastname, email, birthday, password))
+            data.write()
+            print("Come thus far****************************************************************************")
             flash("Thanks for registering!")
             c.close()
             conn.close()
@@ -126,7 +153,6 @@ def register_page():
 
             session['logged_in'] = True
             session['email'] = email
-
             return redirect(url_for('dashboard'))
 
     return render_template("dashboard.html", form = form)
@@ -148,7 +174,7 @@ def create_page():
     return render_template("create.html")
 
 if __name__ == "__main__":
-    app.secret_key = 'super secret key'
+
     app.config['SESSION_TYPE'] = 'filesystem'
 
 
